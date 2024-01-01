1ed7c14501d012a6d7a29af3e177b0ca67cc1250
D4rkKaizen <dima20050111dima@gmail.com>
2024-01-01 18:45:49 +0200

    .

--- D4rkKaizen/1000-7.py
+++ D4rkKaizen/1000-7.py
@@ -1,11 +1,11 @@
 import random
 import time
 
-delay = random.uniform(0.05, 0.2)
+delay = random.uniform(0.03, 0.05)
 
 a=1000
 randomchik= ["сука", "блять", "ненавижу", "твари","ненавижу блять"]
 while a>=0:
   a=a-7
-  print(str(a) + str(" " + random.choice(randomchik)))
+  print(str(a) + str(" | " + random.choice(randomchik)))
   time.sleep(delay)
