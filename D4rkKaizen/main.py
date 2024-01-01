0e9979ac8595c781324c315b968e8f400dccef18
D4rkKaizen <dima20050111dima@gmail.com>
2024-01-01 18:26:21 +0200

    .

--- D4rkKaizen/main.py
+++ D4rkKaizen/main.py
@@ -1,23 +1,4 @@
 # D4rkKaizen
 # print("bober")
 
-# print("--------------------------")
-
-# def aboba(bober):
-#   print(bober)
-
-# aboba("vlas")
-
-import random
-import time
-
-dellay = random.uniform(0.05, 0.2)
-
-a=1000
-randomchik= ["сука", "блять", "ненавижу", "твари","ненавижу блять"]
-while a>=0:
-  a=a-7
-  print(str(a) + str(" " + random.choice(randomchik)))
-  time.sleep(dellay)
-
 
