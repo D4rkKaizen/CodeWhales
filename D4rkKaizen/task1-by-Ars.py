0d745e47397370a8a4d085d518cf080ff81d1c0d
D4rkKaizen <dima20050111dima@gmail.com>
2024-01-01 19:43:40 +0200

    task ars

--- D4rkKaizen/task1-by-Ars.py
+++ D4rkKaizen/task1-by-Ars.py
@@ -1,4 +1,4 @@
-x=input("enter ur number (NOT STRING SUKA!)")
-first = bool(float((x)))
-second = bool(int((x)))+2
+x=input("enter ur variable blyat")
+first = bool(bool((x)))
+second = bool(((x)))+2
 print(str(first)+"\n"+str(second))
