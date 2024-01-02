27037e675e22c9d7e242114c15ce42c3dbf90f02
Vlas <kozlovvlas@gmail.com>
2024-01-02 21:48:40 +0300

    fix task2

--- vlaskozlov/task2-by-ars.py
+++ vlaskozlov/task2-by-ars.py
@@ -1,5 +1,5 @@
 SWD = 'Sweden is the best'
 
-smth_cringe = [SWD, 'а', 'мне', 'американские', 'бургеры', 'дороже', 'родины', input('Vvedi che nit suda:'), input('i suda:')]
+smth_cringe = [SWD, input('Vvedi che nit suda:'), input('i suda:')]
 
 print(smth_cringe)
