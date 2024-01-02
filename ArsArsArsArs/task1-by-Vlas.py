512915e05b321b030ee87fbf8f97e2e14f2abfa2
ArsArsArsArs <galochkaarseny@gmail.com>
2024-01-02 15:37:29 +0300

    vlas norm task sps

---
+++ ArsArsArsArs/task1-by-Vlas.py
@@ -0,0 +1,21 @@
+def isnumber(string: str):
+    return string.isnumeric()
+
+
+def nonumber_error(inp: str):
+    print("You have ented not a row of digits, dummy!")
+
+
+def working(inp: str):
+    inp2 = inp.replace(inp[0], "", 1)
+    inp3 = inp2.replace(inp2[0], "", 1)
+    inp4 = inp3.replace(inp3[0], "", 1)
+    print(inp4)
+
+
+condiditions = {"False": nonumber_error, "True": working}
+
+inp = input("Enter a row of digits: ")
+result_isnumber = isnumber(inp)
+result_conditions = condiditions[str(result_isnumber)]
+result_conditions(inp)
